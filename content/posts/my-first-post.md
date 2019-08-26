189ed81dff7428f53d4f0e73869e12e08391b04e
Dipal Bhavsar <dipal.bhavsar@gmail.com>
2019-08-26 15:29:31 +0530

    Testing
    This is another testing

--- content/posts/my-first-post.md
+++
@@ -1,6 +0,0 @@
----
-title: "My First Post"
-date: 2019-08-22T12:56:56+05:30
-draft: true
----
-
---
+++ precache-manifest.1fc1f475ab9f6700eb54f72aca70cab8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc3772f57ec81a6857b00a22500582c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dea59cd305fdcbde48b",
+    "url": "/static/js/2.178c7371.chunk.js"
+  },
+  {
+    "revision": "3fee3b975cf0247dd4c1",
+    "url": "/static/js/main.e252371c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
