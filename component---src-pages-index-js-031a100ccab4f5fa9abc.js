9abf029448ca8cb3d07e4652eb18f84321584c84
Dipal Bhavsar <dipal.bhavsar@gmail.com>
2020-07-02 22:49:42 +0530

    Updates

--- component---src-pages-index-js-031a100ccab4f5fa9abc.js
+++ component---src-pages-index-js-031a100ccab4f5fa9abc.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[9],{RXBc:function(a,e,t){"use strict";t.r(e),t.d(e,"pageQuery",(function(){return c}));t("q1tI");var n=t("LqO3"),o=t("Bl7J"),r=t("vrFN"),i=t("qKvR");e.default=function(a){var e=a.location,t=a.data.allMarkdownRemark.edges;return Object(i.d)(o.a,{location:e},Object(i.d)(r.a,{title:"Home",description:"List of all available post.",keywords:["coding","programming","Dipal Bhavsar","gatsbyJS","web development","React","web programming"]}),t.map((function(a){var e=a.node;return Object(i.d)(n.a,{blogData:e,key:"_"+e.id})})))};var c="1432908495"}}]);
-//# sourceMappingURL=component---src-pages-index-js-64ab3aae97f76bea8144.js.map
+//# sourceMappingURL=component---src-pages-index-js-031a100ccab4f5fa9abc.js.map
