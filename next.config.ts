ec273a43d4758a56e6affe41367f26e4039fb561
Dipalkumar Bhavsar <dipalkumar.bhavsar@bajajfinserv.in>
2025-01-07 18:17:22 +0530

    updated with old version

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,130 @@
 import type { NextConfig } from "next";
 
+import { withContentlayer } from "next-contentlayer2";
+
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+// You might need to insert additional domains in script-src if you are using external services
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src *.s3.amazonaws.com;
+  connect-src *;
+  font-src 'self';
+  frame-src giscus.app
+`;
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains",
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
+
+const output = process.env.EXPORT ? "export" : undefined;
+const basePath = process.env.BASE_PATH || undefined;
+const unoptimized = process.env.UNOPTIMIZED ? true : undefined;
+
 const nextConfig: NextConfig = {
   /**
    * Enable static exports for the App Router.
    *
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
-  output: "export",
-
+  // output: "export",
   /**
    * Set base path. This is usually the slug of your repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/about",
-
+  // basePath: "/about",
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
    *
    * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
    */
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
-export default nextConfig;
+// export default nextConfig;
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+module.exports = () => {
+  const plugins = [withContentlayer, withBundleAnalyzer];
+  return plugins.reduce((acc: NextConfig, next) => next(acc), {
+    output,
+    basePath,
+    reactStrictMode: true,
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    eslint: {
+      dirs: ["app", "components", "layouts", "scripts"],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "picsum.photos",
+        },
+      ],
+      unoptimized,
+    },
+    async headers() {
+      return [
+        {
+          source: "/(.*)",
+          headers: securityHeaders,
+        },
+      ];
+    },
+    webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+
+      return config;
+    },
+  });
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,96 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-export default {
+// export default {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// } satisfies Config;
+
+// @ts-check
+import { fontFamily } from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+
+/** @type {import("tailwindcss/types").Config } */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/pliny/**/*.js",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,tsx}",
+    "./components/**/*.{js,ts,tsx}",
+    "./layouts/**/*.{js,ts,tsx}",
+    "./data/**/*.mdx",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      lineHeight: {
+        11: "2.75rem",
+        12: "3rem",
+        13: "3.25rem",
+        14: "3.5rem",
       },
+      fontFamily: {
+        sans: ["var(--font-space-grotesk)", ...fontFamily.sans],
+      },
+      colors: {
+        primary: colors.pink,
+        gray: colors.gray,
+      },
+      zIndex: {
+        60: "60",
+        70: "70",
+        80: "80",
+      },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      typography: ({ theme }: { theme: any }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme("colors.primary.500"),
+              "&:hover": {
+                color: `${theme("colors.primary.600")}`,
+              },
+              code: { color: theme("colors.primary.400") },
+            },
+            "h1,h2": {
+              fontWeight: "700",
+              letterSpacing: theme("letterSpacing.tight"),
+            },
+            h3: {
+              fontWeight: "600",
+            },
+            code: {
+              color: theme("colors.indigo.500"),
+            },
+          },
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme("colors.primary.500"),
+              "&:hover": {
+                color: `${theme("colors.primary.400")}`,
+              },
+              code: { color: theme("colors.primary.400") },
+            },
+            "h1,h2,h3,h4,h5,h6": {
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [import("@tailwindcss/forms"), import("@tailwindcss/typography")],
+};
