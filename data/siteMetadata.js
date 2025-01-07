a27fa71457e24b8eaad6beb8187f25ca31372119
Dipalkumar Bhavsar <dipalkumar.bhavsar@bajajfinserv.in>
2025-01-07 19:17:51 +0530

    updated blogs

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,31 +1,31 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Dipal Bhavsar Blog',
-  author: 'Dipal Bhavsar',
-  headerTitle: 'Dipal Bhavsar Blog',
+  title: "Dipal Bhavsar",
+  author: "Dipal Bhavsar",
+  headerTitle: "Dipal Bhavsar",
   description:
-    'This site is developed using React, Next.js, i18n and Tailwind.css. Hosted in GitHub page. This site is intense for self learning and exploring different technologies.',
-  language: 'en-us',
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://www.dipalbhavsar.com',
-  siteRepo: 'https://github.com/dipalbhavsar',
-  siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'info@dipalbhavsar.com',
-  github: 'https://github.com/dipalbhavsar',
-  x: 'https://twitter.com/dipal_bhavsar',
-  // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com/bhavsar.dipal',
-  youtube: 'https://youtube.com/dipalbhavsar',
-  linkedin: 'https://www.linkedin.com/in/dipal-bhavsar',
-  threads: 'https://www.threads.net/dipalbhavsar',
-  instagram: 'https://www.instagram.com/dipal1208',
-  medium: 'https://medium.com',
-  bluesky: 'https://bsky.app/',
-  locale: 'en-US',
+    "This site is developed using React, Next.js, i18n and Tailwind.css. Hosted in GitHub page. This site is intense for self learning and exploring different technologies.",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://www.dipalbhavsar.com",
+  siteRepo: "https://github.com/dipalbhavsar",
+  siteLogo: `${process.env.BASE_PATH || ""}/static/images/logo.png`,
+  socialBanner: `${process.env.BASE_PATH || ""}/static/images/twitter-card.png`,
+  mastodon: "https://mastodon.social/@mastodonuser",
+  email: "info@dipalbhavsar.com",
+  github: "https://github.com/dipalbhavsar",
+  x: "https://twitter.com/dipal_bhavsar",
+  // twitter: "https://twitter.com/Twitter",
+  facebook: "https://facebook.com/bhavsar.dipal",
+  youtube: "https://youtube.com/dipalbhavsar",
+  linkedin: "https://www.linkedin.com/in/dipal-bhavsar",
+  threads: "https://www.threads.net/dipalbhavsar",
+  instagram: "https://www.instagram.com/dipal1208",
+  medium: "https://medium.com",
+  bluesky: "https://bsky.app/",
+  locale: "en-US",
   // set to true if you want a navbar fixed to the top
-  stickyNav: true,
+  stickyNav: false,
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
@@ -33,71 +33,71 @@ const siteMetadata = {
     umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
       umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-      // You may also need to overwrite the script if you're storing data in the US - ex:
-      // src: 'https://us.umami.is/script.js'
-      // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
+      // You may also need to overwrite the script if you"re storing data in the US - ex:
+      // src: "https://us.umami.is/script.js"
+      // Remember to add "us.umami.is" in `next.config.js` as a permitted domain for the CSP
     },
     plausibleAnalytics: {
-      plausibleDataDomain: 'dipalbhavsar.com', // e.g. tailwind-nextjs-starter-blog.vercel.app
+      plausibleDataDomain: "dipalbhavsar.com", // e.g. tailwind-nextjs-starter-blog.vercel.app
     },
     // simpleAnalytics: {},
     // posthogAnalytics: {
-    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    //   posthogProjectApiKey: "", // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: 'UA-167858384-1',
+      googleAnalyticsId: "UA-167858384-1",
     },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: "buttondown",
   },
-  iscomments: false,
+  iscomments: true,
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: "giscus", // supported providers: giscus, utterances, disqus
     giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
+      // Visit the link below, and follow the steps in the "configuration" section
       // https://giscus.app/
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      mapping: "pathname", // supported options: pathname, url, title
+      reactions: "1", // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
+      metadata: "0",
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: "light",
       // theme when dark mode
-      darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
+      darkTheme: "transparent_dark",
+      // If the theme option above is set to "custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      themeURL: "",
+      // This corresponds to the `data-lang="en"` in giscus"s configurations
+      lang: "en",
     },
   },
   search: {
-    provider: 'kbar', // kbar or algolia
+    provider: "kbar", // kbar or algolia
     kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
+      searchDocumentsPath: `${process.env.BASE_PATH || ""}/search.json`, // path to load documents to search
     },
-    // provider: 'algolia',
+    // provider: "algolia",
     // algoliaConfig: {
     //   // The application ID provided by Algolia
-    //   appId: 'R2IYF7ETH7',
+    //   appId: "R2IYF7ETH7",
     //   // Public API key: it is safe to commit it
-    //   apiKey: '599cec31baffa4868cae4e79f180729b',
-    //   indexName: 'docsearch',
+    //   apiKey: "599cec31baffa4868cae4e79f180729b",
+    //   indexName: "docsearch",
     // },
   },
-}
+};
 
-module.exports = siteMetadata
+module.exports = siteMetadata;
